f2c7d72c89619d31d4a75fdb283309cbb5fcb952
nurlan1234nur <nurlant566@gmail.com>
2026-04-27 11:46:35 +0800

    fix: import existing security group rules into terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,75 @@ import {
   id = "sg-04d4185c11e869327"
 }
 
+# Import control SG ingress rules
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-22-22-0.0.0.0_0"]
+  id = "sgr-0f55a71dad58cc0ff"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-80-80-0.0.0.0_0"]
+  id = "sgr-0202f3e645fb06919"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-443-443-0.0.0.0_0"]
+  id = "sgr-07a92f17c57465f0e"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-6443-6443-0.0.0.0_0"]
+  id = "sgr-06181ab6552ca0d6a"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-3000-3003-0.0.0.0_0"]
+  id = "sgr-0c3506074ebb842b1"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.control["-1-0-0-172.31.0.0_16"]
+  id = "sgr-0a9b55bade1d7112e"
+}
+
+# Import control SG egress rule
+import {
+  to = module.security.aws_vpc_security_group_egress_rule.control["-1-0-0-0.0.0.0_0"]
+  id = "sgr-0b0185b5c69930201"
+}
+
+# Import worker SG ingress rules
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.worker["tcp-22-22-0.0.0.0_0"]
+  id = "sgr-024a75127b5f2f989"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.worker["tcp-80-80-0.0.0.0_0"]
+  id = "sgr-0405cea982f019c0b"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.worker["tcp-443-443-0.0.0.0_0"]
+  id = "sgr-0ab19155060499950"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.worker["tcp-6443-6443-0.0.0.0_0"]
+  id = "sgr-00a4193c0399cf04b"
+}
+
+import {
+  to = module.security.aws_vpc_security_group_ingress_rule.worker["-1-0-0-172.31.0.0_16"]
+  id = "sgr-0feeefda91d05591d"
+}
+
+# Import worker SG egress rule
+import {
+  to = module.security.aws_vpc_security_group_egress_rule.worker["-1-0-0-0.0.0.0_0"]
+  id = "sgr-07c65a2031b4c42c7"
+}
+
 # Import existing EC2 instances into Terraform state
 import {
   to = module.compute.aws_instance.control
