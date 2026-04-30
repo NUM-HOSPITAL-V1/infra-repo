d2002b918566ecd96dd68e4289f67b09f25b14c5
nurlan1234nur <nurlant566@gmail.com>
2026-04-30 22:27:55 +0800

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,79 +35,10 @@ module "compute" {
 # Import existing security groups into Terraform state
 import {
   to = module.security.aws_security_group.control
-  id = "sg-0a7805255b846aec8"
+  id = "sg-0ee9c796e766b0d4f"
 }
 
 import {
   to = module.security.aws_security_group.worker
-  id = "sg-04d4185c11e869327"
-}
-
-# Import control SG ingress rules
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-22-22-0.0.0.0_0"]
-  id = "sgr-0f55a71dad58cc0ff"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-80-80-0.0.0.0_0"]
-  id = "sgr-0202f3e645fb06919"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-443-443-0.0.0.0_0"]
-  id = "sgr-07a92f17c57465f0e"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-6443-6443-0.0.0.0_0"]
-  id = "sgr-06181ab6552ca0d6a"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.control["tcp-3000-3003-0.0.0.0_0"]
-  id = "sgr-0c3506074ebb842b1"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.control["-1-0-0-172.31.0.0_16"]
-  id = "sgr-0a9b55bade1d7112e"
-}
-
-# Import control SG egress rule
-import {
-  to = module.security.aws_vpc_security_group_egress_rule.control["-1-0-0-0.0.0.0_0"]
-  id = "sgr-0b0185b5c69930201"
-}
-
-# Import worker SG ingress rules
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.worker["tcp-22-22-0.0.0.0_0"]
-  id = "sgr-024a75127b5f2f989"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.worker["tcp-80-80-0.0.0.0_0"]
-  id = "sgr-0405cea982f019c0b"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.worker["tcp-443-443-0.0.0.0_0"]
-  id = "sgr-0ab19155060499950"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.worker["tcp-6443-6443-0.0.0.0_0"]
-  id = "sgr-00a4193c0399cf04b"
-}
-
-import {
-  to = module.security.aws_vpc_security_group_ingress_rule.worker["-1-0-0-172.31.0.0_16"]
-  id = "sgr-0feeefda91d05591d"
-}
-
-# Import worker SG egress rule
-import {
-  to = module.security.aws_vpc_security_group_egress_rule.worker["-1-0-0-0.0.0.0_0"]
-  id = "sgr-07c65a2031b4c42c7"
+  id = "sg-0ca594dfe71710a25"
 }
